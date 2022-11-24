5496628c5f80c38213b57bc09b7502e0e363237c
lalankumar001 <tocsi.org@gmail.com>
2022-11-24 13:07:25 +0530

    bhai pareshanhogya

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact kAR le kiya kar raha bhai yai sab doglapan h ram ram karjajljafjdfjadjf </div>
+    <div>Contact kAR le kiya kar raha bhai yai sab doglapan h ram ram karjajljafjdfjadjf 
+      <h3>ram ram ji yahi h </h3>
+    </div>
   )
 }
 
